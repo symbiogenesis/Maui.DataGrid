a8d86632b0d3a726bd00006cef38759b39e6dd4b
Edward Miller <symbiogenesis@outlook.com>
2023-02-15 21:24:04 -0600

    log unhandled exceptions to console

--- Maui.DataGrid.Sample/Platforms/Android/MainActivity.cs
+++ Maui.DataGrid.Sample/Platforms/Android/MainActivity.cs
@@ -7,4 +7,21 @@ using global::Android.OS;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+        TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+        base.OnCreate(savedInstanceState);
+    }
+
+    private static void TaskSchedulerOnUnobservedTaskException(object? obj, UnobservedTaskExceptionEventArgs args) => Console.WriteLine(args.Exception.Message);
+
+    private static void CurrentDomainOnUnhandledException(object? obj, UnhandledExceptionEventArgs args)
+    {
+        if (args.ExceptionObject is Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
 }
